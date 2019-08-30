7e990f88a910a2f63358db31b6643a1d94169975
Hossain Amrin <hossainamrin4@gmail.com>
2019-08-30 21:35:15 +0600

    Update Code_with_LCD.ino

--- Code_with_LCD.ino
+++ Code_with_LCD.ino
@@ -1,45 +1,66 @@
-#include <LiquidCrystal.h>
 
-LiquidCrystal lcd(6 , 7, 5, 4, 3, 2);
+#include < LiquidCrystal.h >
+
+    LiquidCrystal lcd(6, 7, 5, 4, 3, 2);
 const int trigPin = 11;
 const int echoPin = 10;
-long duration,cm,inches;
+long duration, cm, inches;
 int distanceCm, distanceInch;
 
 void setup() {
-  
-lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-Serial.begin (9600);
+
+    lcd.begin(16, 2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
 
-duration = pulseIn(echoPin, HIGH);
-distanceCm= duration*0.034/2;
-distanceInch = duration*0.0133/2;
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
-lcd.print("Distance: "); // Prints string "Distance" on the LCD
-lcd.print(distanceCm); // Prints the distance value from the sensor
-lcd.print("  cm");
-delay(10);
-lcd.setCursor(0,1);
-lcd.print("Distance: ");
-lcd.print(distanceInch);
-lcd.print("inch");
-Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-delay(10);
+    duration = pulseIn(echoPin, HIGH);
+    distanceCm = duration * 0.034 / 2;
+    distanceInch = duration * 0.0133 / 2;
 
+    lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+    lcd.print("Distance: "); // Prints string "Distance" on the LCD
+    lcd.print(distanceCm); // Prints the distance value from the sensor
+    lcd.print("  cm");
+    delay(10);
+    lcd.setCursor(0, 1);
+    lcd.print("Distance: ");
+    lcd.print(distanceInch);
+    lcd.print("inch");
+    Serial.print(inches);
+    Serial.print("in, ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+    delay(10);
+
+}
+
+if ((distanceCm > 1)) {
+    digitalWrite(LED1, HIGH);
+
+} else {
+    digitalWrite(LED1, LOW);
+}
+
+if ((distanceCm > 0)) {
+    digitalWrite(LED2, HIGH);
+} else {
+    digitalWrite(LED2, LOW);
+}
+
+if ((distanceCm < 8) || (distanceCm > 300)) {
+    analogWrite(LED, 100);
+
+} else {
+    analogWrite(LED, 255);
 }
